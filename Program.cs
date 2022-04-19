50bea2a8d212e8a70b648f4e9c50b96b84f350a8
muratpala007 <muratpala007@gmail.com>
2022-04-19 18:32:06 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _73DersInterfaces // Interfacesler cokca kullanilir. Abstractlarin gelistirilmis halidir. Yani arayuz cizimleri icin kullanilir.
+                            // Baska siniflardan miras alinir ve miras alindiktan sonra ilgili metotlar turev siniflarda override edilmeye zorlanir...
+{
+    class Program
+    {
+        public interface ISekil
+        {
+            void SekilCiz();
+        }
+
+        class Dikdortgen : ISekil
+        {
+            public void SekilCiz()
+            {
+                Console.WriteLine("Dikdortgen ciziliyor...");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Dikdortgen d1 = new Dikdortgen();
+            d1.SekilCiz();
+        }
+    }
+}
